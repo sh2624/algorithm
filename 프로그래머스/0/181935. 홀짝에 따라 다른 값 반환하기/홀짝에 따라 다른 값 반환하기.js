83dc9baeb4e60af03e3dae6765f83e8f61775c46
Lim Sanghyun <sh2624@daum.net>
2026-04-13 12:52:05 +0900

    [level 0] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.15 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    let answer = 0;
+    
+    if (n % 2 == 1)
+        for (let i = 1; i <= n; i += 2)
+            answer += i;
+    else
+        for (let i = 0; i <= n; i += 2)
+            answer += i * i;
+    
+    return answer;
+}
