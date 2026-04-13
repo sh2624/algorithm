7fca0046608022403e13fd8abca8928c0231475e
Lim Sanghyun <sh2624@daum.net>
2026-04-13 15:23:25 +0900

    [level 0] Title: 조건 문자열, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.js
@@ -0,0 +1,14 @@
+function solution(ineq, eq, n, m) {
+    if (ineq === '>' && eq === '=')
+        if (n >= m) return 1;
+        else return 0;
+    else if (ineq === '<' && eq === '=')
+        if (n <= m) return 1;
+        else return 0;
+    else if (ineq === '>' && eq === '!')
+        if (n > m) return 1;
+        else return 0;
+    else
+        if (n < m) return 1;
+        else return 0;
+}
