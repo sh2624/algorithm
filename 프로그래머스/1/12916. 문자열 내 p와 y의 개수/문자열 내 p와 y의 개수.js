b740076778dbafa36195f97dada90d7b215c1382
Lim Sanghyun <sh2624@daum.net>
2026-06-06 21:08:58 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.13 ms, Memory: 32.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
@@ -0,0 +1,9 @@
+function solution(s){
+    let p = 0, y = 0;
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === 'p' || s[i] === 'P') p++;
+        else if (s[i] === 'y' || s[i] === 'Y') y++;
+    }
+    
+    return (p === y);
+}
