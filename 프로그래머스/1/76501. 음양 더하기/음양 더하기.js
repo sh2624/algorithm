d2013c1f6f997d3005956aa759923f28000734fc
Lim Sanghyun <sh2624@daum.net>
2026-06-07 20:58:09 +0900

    [level 1] Title: 음양 더하기, Time: 0.07 ms, Memory: 32.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -0,0 +1,9 @@
+function solution(absolutes, signs) {
+    let res = 0;
+    for (let i = 0; i < absolutes.length; i++) {
+        if (signs[i]) res += absolutes[i];
+        else res -= absolutes[i];
+    }
+    
+    return res;
+}
