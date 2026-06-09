058e66afa60462bbc5e2aa2f10d01983a238ac39
Lim Sanghyun <sh2624@daum.net>
2026-06-09 20:32:53 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.10 ms, Memory: 44 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,11 @@
+function solution(num) {
+    for (let i = 0; i < 500; i++) {
+        if (num === 1) return i;
+        else {
+            if (num % 2 === 0) num /= 2;
+            else num = num * 3 + 1;
+        }
+    }
+    
+    return -1;
+}
